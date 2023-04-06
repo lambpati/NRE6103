cf32c226bbd5c3e4d02d68168e98553202e99ac1
Patience Yockey <lambpati@isu.edu>
2023-04-06 14:51:32 -0400

    Forgot some stuff

--- src/transporter.cc
+++ src/transporter.cc
@@ -4,7 +4,7 @@ void Transporter::russianRoulette(Particle& p){
     //Roulettes weight of the particle 
     if (std::abs(p.wgt) < weight_cutoff) {
         double P_kill = 1.0 - (std::abs(p.wgt) / survive_weight);
-        if (RNG::rand() < P_kill)
+        if (RNG::rand() < P_kill) {
             //Lost the game of Russian Roulette
             p.wgt = 0.;
             p.is_alive = false;
@@ -13,4 +13,5 @@ void Transporter::russianRoulette(Particle& p){
             //Survived another day
             p.wgt = std::copysign(survive_weight, p.wgt);
         }
+    }
 }
