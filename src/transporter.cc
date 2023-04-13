ddb0a88d96bace36110b20e48343962304e5371c
Patience Yockey <lambpati@isu.edu>
2023-04-13 09:46:34 -0400

    Changed the transporter code for determineMaterial

--- src/transporter.cc
+++ src/transporter.cc
@@ -48,30 +48,22 @@ void Transporter::russianRoulette(Particle& p){
 void Transporter::determineMaterial(Particle& p, int i){
     // REMEMBER i starts at 1
     // Check to make sure particle position is within i'th region bounds (Region = index + 1)
-    if(p.pos < Geometry::getXSvRegion(i).X_max && p.pos >= Geometry::getXSvRegion(i).X_min){
-        current_region = Geometry::getRegion(i-1);
-        return;
-    }
-    // If out of i'th region, iterate + 1 to find correct region
-    else if(p.pos >= Geometry::getXSvRegion(i).X_max && p.pos <= Geometry::getMax()){
-        Transporter::determineMaterial(p, i+1);
-    }
-    // If before i'th region, iterate - 1 to find correct region
-    else if(p.pos < Geometry::getXSvRegion(i).X_min && p.pos >= Geometry::getMin()){
-        Transporter::determineMaterial(p, i-1);
+    for (int i = 0; i < Geometry::getGeometry().size(); i++) {
+        if (p.pos < Geometry::getXSvRegion(i+1).X_max && p.pos >= Geometry::getXSvRegion(i+1).X_min) {
+            current_region = Geometry::getRegion(i-1);
+            return;
+        }
+
     }
     // Fall through case. particle outside of material. Particle is determined by BCs
-    else{
         // LHS BC
-        if(p.pos < Geometry::getMin()){
+        if(p.pos <= Geometry::getMin()){
             Boundaries::determineBehavior(p, false);
         }
         // RHS BC
         else{
             Boundaries::determineBehavior(p, true);
         }
-
-    }
 }
 
 void Transporter::moveParticle(Particle& p){
